3aeb4e37cb5c1b376a4db017a7ca61f5e858c5a2
ngodings <50858027+ngodings@users.noreply.github.com>
2019-12-24 13:00:35 +0700

    1

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
   <title>PESANTREN</title>
 
   <!-- Custom fonts for this template-->
-  <link href="template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="modul/template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Page level plugin CSS-->
-  <link href="template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <link href="modul/template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="template/css/sb-admin.css" rel="stylesheet">
+  <link href="modul/template/css/sb-admin.css" rel="stylesheet">
 
 </head>
 
@@ -26,32 +26,45 @@
 
 <div class="container">
     <div class="card card-register mx-auto mt-5">
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="">USTAD</a>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/ustad">USTAD</a>
     </div>
     <div class="card card-register mx-auto mt-5">
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="">SANTRI</a>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/santri">SANTRI</a>
     </div>
 </div>  
 
+<!-- <?php
+    include "conn.php";
+
+    if(isset($_GET['modul'])){
+      if($_GET['modul'] == 'santri'){
+        include "./modul/santri/index.php";
+        
+      }else if($_GET['modul'] == 'ustad'){
+        include "./modul/ustad/index.php";
+      }
+    }
+?> -->
 
 
-  <script src="template/vendor/jquery/jquery.min.js"></script>
-  <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="modul/template/vendor/jquery/jquery.min.js"></script>
+  <script src="modul/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="modul/template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Page level plugin JavaScript-->
-  <script src="template/vendor/chart.js/Chart.min.js"></script>
-  <script src="template/vendor/datatables/jquery.dataTables.js"></script>
-  <script src="template/vendor/datatables/dataTables.bootstrap4.js"></script>
+  <script src="modul/template/vendor/chart.js/Chart.min.js"></script>
+  <script src="modul/template/vendor/datatables/jquery.dataTables.js"></script>
+  <script src="modul/template/vendor/datatables/dataTables.bootstrap4.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="template/js/sb-admin.min.js"></script>
+  <script src="modul/template/js/sb-admin.min.js"></script>
 
   <!-- Demo scripts for this page-->
-  <script src="template/js/demo/datatables-demo.js"></script>
-  <script src="template/js/demo/chart-area-demo.js"></script>
+  <script src="modul/template/js/demo/datatables-demo.js"></script>
+  <script src="modul/template/js/demo/chart-area-demo.js"></script>
 
 </body>
 </html>
--- modul/santri/index.php
+++ modul/santri/index.php
@@ -12,13 +12,13 @@
   <title>SB Admin - Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Page level plugin CSS-->
-  <link href="template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <link href="../template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="template/css/sb-admin.css" rel="stylesheet">
+  <link href="../template/css/sb-admin.css" rel="stylesheet">
 
 </head>
 
@@ -26,7 +26,7 @@
 
   <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.php">Pesantren</a>
+    <a class="navbar-brand mr-1" href="index.php">Start Bootstrap</a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
@@ -102,22 +102,22 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Login Screens:</h6>
-          <a class="dropdown-item" href="template/login.html">Login</a>
-          <a class="dropdown-item" href="template/register.html">Register</a>
-          <a class="dropdown-item" href="template/forgot-password.html">Forgot Password</a>
+          <a class="dropdown-item" href="../template/login.html">Login</a>
+          <a class="dropdown-item" href="../template/register.html">Register</a>
+          <a class="dropdown-item" href="../template/forgot-password.html">Forgot Password</a>
           <div class="dropdown-divider"></div>
           <h6 class="dropdown-header">Other Pages:</h6>
-          <a class="dropdown-item" href="template/404.html">404 Page</a>
-          <a class="dropdown-item" href="template/blank.html">Blank Page</a>
+          <a class="dropdown-item" href="../template/404.html">404 Page</a>
+          <a class="dropdown-item" href="../template/blank.html">Blank Page</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="template/charts.html">
+        <a class="nav-link" href="../template/charts.html">
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Charts</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="template/tables.html">
+        <a class="nav-link" href="../template/tables.html">
           <i class="fas fa-fw fa-table"></i>
           <span>Tables</span></a>
       </li>
@@ -742,30 +742,30 @@
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="template/login.html">Logout</a>
+          <a class="btn btn-primary" href="../template/login.html">Logout</a>
         </div>
       </div>
     </div>
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="template/vendor/jquery/jquery.min.js"></script>
-  <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../template/vendor/jquery/jquery.min.js"></script>
+  <script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="../template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Page level plugin JavaScript-->
-  <script src="template/vendor/chart.js/Chart.min.js"></script>
-  <script src="template/vendor/datatables/jquery.dataTables.js"></script>
-  <script src="template/vendor/datatables/dataTables.bootstrap4.js"></script>
+  <script src="../template/vendor/chart.js/Chart.min.js"></script>
+  <script src="../template/vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../template/vendor/datatables/dataTables.bootstrap4.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="template/js/sb-admin.min.js"></script>
+  <script src="../template/js/sb-admin.min.js"></script>
 
   <!-- Demo scripts for this page-->
-  <script src="template/js/demo/datatables-demo.js"></script>
-  <script src="template/js/demo/chart-area-demo.js"></script>
+  <script src="../template/js/demo/datatables-demo.js"></script>
+  <script src="../template/js/demo/chart-area-demo.js"></script>
 
 </body>
 
