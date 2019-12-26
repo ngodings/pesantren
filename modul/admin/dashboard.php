77606c7e2f84f50f2eabe20bf6a02e1260d34b2d
ngodings <50858027+ngodings@users.noreply.github.com>
2019-12-26 23:57:17 +0700

    contoh dashboard ustad

--- modul/admin/dashboard.php
+++ modul/admin/dashboard.php
@@ -8,4 +8,49 @@ if(!isset($_SESSION['email'])) {
 
 }
 ?>
-ini admin
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>DASHBOARD ADMIN</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="../template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../template/css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<script src="../template/vendor/jquery/jquery.min.js"></script>
+  <script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../template/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Page level plugin JavaScript-->
+  <script src="../template/vendor/chart.js/Chart.min.js"></script>
+  <script src="../template/vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../template/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../template/js/sb-admin.min.js"></script>
+
+  <!-- Demo scripts for this page-->
+  <script src="../template/js/demo/datatables-demo.js"></script>
+  <script src="../template/js/demo/chart-area-demo.js"></script>
+
+</body>
+</html>
--- modul/ustad/dashboard.php
+++ modul/ustad/dashboard.php
@@ -8,4 +8,102 @@ if(!isset($_SESSION['email'])) {
 
 }
 ?>
-ini dashboard ustad
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>WELCOME USTAD</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="../template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../template/css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+    <a class="navbar-brand mr-1" href="dashboard.php">DASHBOARD USTAD</a>
+
+    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Navbar -->
+    <ul class="navbar-nav ml-auto mr-0">
+      <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle fa-fw"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+        </div>
+      </li>
+    </ul>
+
+  </nav>
+
+<div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="#">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li class="nav-item">
+        <a class="nav-link" href="hafalan.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Hafalan</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="softskill.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Softskill</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pesan.php">
+          <i class="fas fa-fw fa-mail-bulk"></i>
+          <span>Pesan</span></a>
+      </li>
+</ul>
+</div>
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+<script src="../template/vendor/jquery/jquery.min.js"></script>
+  <script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../template/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Page level plugin JavaScript-->
+  <script src="../template/vendor/chart.js/Chart.min.js"></script>
+  <script src="../template/vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../template/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../template/js/sb-admin.min.js"></script>
+
+  <!-- Demo scripts for this page-->
+  <script src="../template/js/demo/datatables-demo.js"></script>
+  <script src="../template/js/demo/chart-area-demo.js"></script>
+
+</body>
+</html>
