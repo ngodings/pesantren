671f0c3fca94f3a51868c9970f7fd51a4f579ce4
tflnurmad24 <59126842+tflnurmad24@users.noreply.github.com>
2019-12-27 14:28:57 +0700

    Create hafalan.php

---
+++ modul/ustad/hafalan.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>WELCOME USTAD</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="../template/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../template/css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+    <a class="navbar-brand mr-1" href="dashboard.php">DASHBOARD USTAD</a>
+
+    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Navbar -->
+    <ul class="navbar-nav ml-auto mr-0">
+      <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle fa-fw"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+        </div>
+      </li>
+    </ul>
+
+  </nav>
+
+<div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="sidebar navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="#">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li class="nav-item">
+        <a class="nav-link" href="hafalan.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Hafalan</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="softskill.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Softskill</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pesan.php">
+          <i class="fas fa-fw fa-mail-bulk"></i>
+          <span>Pesan</span></a>
+      </li>
+</ul>
+  <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Overview</li>
+        </ol>
+
+        <!-- Icon Cards-->
+        <div class="row">
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-warning o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5">Tambah Data baru</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <!-- DataTables Example -->
+        <div class="card mb-3">
+          <div class="card-header">
+            <i class="fas fa-table"></i>
+            Data Table Example</div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>Surah</th>
+                    <th>Juz</th>
+                    <th>Ayat</th>
+                    <th>Halaman</th>
+                    <th>Total hafalan</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>Surah</th>
+                    <th>Juz</th>
+                    <th>Ayat</th>
+                    <th>Halaman</th>
+                    <th>Total lembar</th>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  <tr>
+                    <td>An-nas</td>
+                    <td>30</td>
+                    <td>6</td>
+                    <td>21</td>
+                    <td>10</td>                
+                  </tr>
+                  <tr>
+                    <td>Al-falaq</td>
+                    <td>30</td>
+                    <td>5</td>
+                    <td>21</td>
+                    <td>10</td>   
+                  </tr>
+                  <tr>
+                    <td>Al-ikhlas</td>
+                    <td>30</td>
+                    <td>4</td>
+                    <td>21</td>
+                    <td>10</td>   
+                  </tr>                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
+
+      </div>
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+<script src="../template/vendor/jquery/jquery.min.js"></script>
+  <script src="../template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../template/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Page level plugin JavaScript-->
+  <script src="../template/vendor/chart.js/Chart.min.js"></script>
+  <script src="../template/vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../template/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../template/js/sb-admin.min.js"></script>
+
+  <!-- Demo scripts for this page-->
+  <script src="../template/js/demo/datatables-demo.js"></script>
+  <script src="../template/js/demo/chart-area-demo.js"></script>
+
+</body>
+</html>
