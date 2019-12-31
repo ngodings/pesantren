167361445e750c25b6a0f8f0049b5c1eb83ca37c
tflnurmad24 <59126842+tflnurmad24@users.noreply.github.com>
2019-12-31 14:11:28 +0700

    template2

--- index.php
+++ index.php
@@ -19,24 +19,115 @@
 
   <!-- Custom styles for this template-->
   <link href="modul/template/css/sb-admin.css" rel="stylesheet">
+  
+  <!-- ** Plugins Needed for the Project ** -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="modul/template2/plugins/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/themify/css/themify-icons.css">
+  <link rel="stylesheet" href="modul/template2/plugins/icofont/icofont.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/fontawesome/css/all.css">
+  <link rel="stylesheet" href="modul/template2/plugins/aos/aos.css">
+  <link rel="stylesheet" href="modul/template2/plugins/magnific-popup/magnific-popup.css">
+  <link rel="stylesheet" href="modul/template2/plugins/video-popup/modal-video.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/swiper/swiper.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/date-picker/datepicker.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/clock-picker/clockpicker.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/bootstrap-touchpin/jquery.bootstrap-touchspin.min.css">
+  <link rel="stylesheet" href="modul/template2/plugins/devices.min.css">
+
+  <!-- Main Stylesheet -->
+  <link href="modul/template2/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
-<center><img src="modul/template/img/Althufeil.jpg"></center>
-<div class="container">
-    <div class="card card-register mx-auto mt-5">
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/ustad">USTAD</a>
+<!--  Banner start -->
+<section class="slider-hero hero-slider  hero-style-1  ">
+  <div class="swiper-container swiper-container-horizontal">
+    <div class="swiper-wrapper">
+      <!-- start slide-item -->
+      <div class="swiper-slide slide-item">
+        <div class="slide-inner slide-bg-image main-sider-inner" data-background="modul/template2/images/banner/pesantren.jpg">
+          <!-- <div class="overlay"></div> -->
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-7">
+                <span data-swiper-parallax="300" class="text-primary font-extra font-md">Selamat Datang</span>
+                <h1 class="mt-3 mb-5 text-lg text-white" data-swiper-parallax="400">Silahkan pilih akses yang bisa anda masuki, Terimakasih</h1>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/ustad">USTAD</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/santri">SANTRI</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/admin">ADMIN</a>
+					</div>
+			  </div>
+            </div>			
+          </div>
+        </div> 
+      </div>
+      <!-- end slide-item -->
+	  
+	  <!-- start slide-item -->
+      <div class="swiper-slide slide-item">
+        <div class="slide-inner slide-bg-image main-sider-inner" data-background="modul/template2/images/banner/masjid.jpg">
+          <!-- <div class="overlay"></div> -->
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-7">
+                <span data-swiper-parallax="300" class="text-primary font-extra font-md">Selamat Datang</span>
+                <h1 class="mt-3 mb-5 text-lg text-white" data-swiper-parallax="400">Silahkan pilih akses yang bisa anda masuki, Terimakasih</h1>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/ustad">USTAD</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/santri">SANTRI</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/admin">ADMIN</a>
+					</div>
+			  </div>
+            </div>			
+          </div>
+        </div> 
+      </div>
+      <!-- end slide-item -->
+	  
+	  <!-- start slide-item -->
+      <div class="swiper-slide slide-item">
+        <div class="slide-inner slide-bg-image main-sider-inner" data-background="modul/template2/images/banner/Ranjang.jpeg">
+          <!-- <div class="overlay"></div> -->
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-7">
+                <span data-swiper-parallax="300" class="text-primary font-extra font-md">Selamat Datang</span>
+                <h1 class="mt-3 mb-5 text-lg text-white" data-swiper-parallax="400">Silahkan pilih akses yang bisa anda masuki, Terimakasih</h1>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/ustad">USTAD</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/santri">SANTRI</a>
+					</div>
+					<div class="card card-register mx-auto mt-5">
+						<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/admin">ADMIN</a>
+					</div>
+			  </div>
+            </div>			
+          </div>
+        </div> 
+      </div>
+      <!-- end slide-item -->
+	  
     </div>
-    <div class="card card-register mx-auto mt-5">
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/santri">SANTRI</a>
-    </div>
-    <div class="card card-register mx-auto mt-5">
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="modul/admin">ADMIN</a>
-    </div>
-</div>  
-
-
+    <!-- end swiper-wrapper -->
+    <!-- swipper controls -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+</section>
+<!--  Banner End -->
 
 
   <script src="modul/template/vendor/jquery/jquery.min.js"></script>
@@ -56,6 +147,27 @@
   <!-- Demo scripts for this page-->
   <script src="modul/template/js/demo/datatables-demo.js"></script>
   <script src="modul/template/js/demo/chart-area-demo.js"></script>
+  
+  <!-- jQuery -->
+<script src="modul/template2/plugins/jQuery/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="modul/template2/plugins/bootstrap/bootstrap.min.js"></script>
+<script src="modul/template2/plugins/aos/aos.js"></script>
+<script src="modul/template2/plugins/shuffle/shuffle.min.js"></script>
+<script src="modul/template2/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="modul/template2/plugins/date-picker/datepicker.min.js"></script>
+<script src="modul/template2/plugins/clock-picker/clockpicker.min.js"></script>
+<script src="modul/template2/plugins/video-popup/jquery-modal-video.min.js"></script>
+<script src="modul/template2/plugins/swiper/swiper.min.js"></script>
+<script src="modul/template2/plugins/instafeed/instafeed.min.js"></script>
+<script src="modul/template2/plugins/bootstrap-touchpin/jquery.bootstrap-touchspin.min.js"></script>
+
+ <!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script> 
+<script src="modul/template2/plugins/google-map/gmap.js"></script>
+<!-- Main Script -->
+<script src="modul/template2/js/contact.js"></script>
+<script src="modul/template2/js/script.js"></script>
 
 </body>
 </html>
