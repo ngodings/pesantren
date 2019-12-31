1b0402c9acdb6e94fe8615e1d7da1ae739f480ab
ngodings <50858027+ngodings@users.noreply.github.com>
2019-12-31 12:32:48 +0700

    baru

--- pesantren.sql
+++ pesantren.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Des 2019 pada 14.45
+-- Waktu pembuatan: 31 Des 2019 pada 06.30
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.2
 
@@ -63,21 +63,29 @@ CREATE TABLE `db_tbl_hafalan` (
 --
 
 INSERT INTO `db_tbl_hafalan` (`id_hafalan`, `tanggal_hafalan`, `surah`, `juz`, `pencapaian_hafalan`, `id_santri`, `id_ustad`) VALUES
-('HA00001', '2019-09-12', 'Al - Baqarah', 1, 2, 'SANTR0001', 'UST0004');
+('HA00001', '2019-09-12', 'Al - Baqarah', 1, 2, 'SANTR0001', 'UST0004'),
+('HA00002', '2019-09-17', 'Al - Baqarah', 1, 3, 'SANTR0001', 'UST0004');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `db_tbl_pesan`
+-- Struktur dari tabel `db_tbl_pelanggaran`
 --
 
-CREATE TABLE `db_tbl_pesan` (
-  `id_pesan` varchar(10) NOT NULL,
-  `id_pengirim` varchar(10) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `email` int(255) NOT NULL,
-  `pesan` tinytext NOT NULL
+CREATE TABLE `db_tbl_pelanggaran` (
+  `id_pelanggaran` varchar(10) NOT NULL,
+  `id_santri` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `pelanggaran` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `db_tbl_pelanggaran`
+--
+
+INSERT INTO `db_tbl_pelanggaran` (`id_pelanggaran`, `id_santri`, `tanggal`, `pelanggaran`, `keterangan`) VALUES
+('PG00001', 'SANTR0001', '2019-09-09', 'Merokok', 'Pada malam hari');
 
 -- --------------------------------------------------------
 
@@ -102,7 +110,7 @@ INSERT INTO `db_tbl_santri` (`id_santri`, `nama`, `kelas`, `asal`, `email`, `pas
 ('SANTR0001', 'RIko Isnaini', '10', 'boyolali', 'riko@gmail.com', 'riko'),
 ('SANTR0002', 'Ritonawa Uga', '10', 'batam', 'uga@mail.com', 'elek'),
 ('SANTR0012', 'lala manis', '12', 'omahlu', 'omah@mail.com', 'sisiap'),
-('SANTR0129', 'emang', '12', 'opo iyo', 'riko1@gmail.com', 'santri');
+('SANTR0111', 'Ryanito Filza', '12', 'Karanganyar', 'ito@santri.com', 'itoito');
 
 -- --------------------------------------------------------
 
@@ -149,8 +157,9 @@ CREATE TABLE `db_tbl_ustad` (
 INSERT INTO `db_tbl_ustad` (`id_ustad`, `nama`, `nip`, `password`, `email`) VALUES
 ('UST0001', 'Tufel', 1234567890, '12345', 'tufel@staff.pesantren.com'),
 ('UST0002', 'riski', 109432294, '12345', 'riski@admin.com'),
-('UST0003', 'Risky Miusdi', 2147483647, 'riski', 'riski@ustad.com'),
-('UST0004', 'Tufeil Noor', 987654321, 'tufeil', 'tufeil@ustad.com');
+('UST0003', 'Risky Miusdi', 2147483647, '12345', 'riski@ustad.com'),
+('UST0004', 'Tufeil Noor', 987654321, 'tufeil', 'tufeil@ustad.com'),
+('UST0005', 'Rendi Jaka', 2147483647, 'rendi', 'rendi@ustad.com');
 
 --
 -- Indexes for dumped tables
@@ -171,11 +180,11 @@ ALTER TABLE `db_tbl_hafalan`
   ADD KEY `id_ustad` (`id_ustad`);
 
 --
--- Indeks untuk tabel `db_tbl_pesan`
+-- Indeks untuk tabel `db_tbl_pelanggaran`
 --
-ALTER TABLE `db_tbl_pesan`
-  ADD PRIMARY KEY (`id_pesan`),
-  ADD KEY `id_pengguna` (`id_pengirim`);
+ALTER TABLE `db_tbl_pelanggaran`
+  ADD PRIMARY KEY (`id_pelanggaran`),
+  ADD KEY `id_santri` (`id_santri`);
 
 --
 -- Indeks untuk tabel `db_tbl_santri`
@@ -209,11 +218,10 @@ ALTER TABLE `db_tbl_hafalan`
   ADD CONSTRAINT `db_tbl_hafalan_ibfk_2` FOREIGN KEY (`id_ustad`) REFERENCES `db_tbl_ustad` (`id_ustad`);
 
 --
--- Ketidakleluasaan untuk tabel `db_tbl_pesan`
+-- Ketidakleluasaan untuk tabel `db_tbl_pelanggaran`
 --
-ALTER TABLE `db_tbl_pesan`
-  ADD CONSTRAINT `db_tbl_pesan_ibfk_1` FOREIGN KEY (`id_pengirim`) REFERENCES `db_tbl_ustad` (`id_ustad`),
-  ADD CONSTRAINT `db_tbl_pesan_ibfk_2` FOREIGN KEY (`id_pengirim`) REFERENCES `db_tbl_santri` (`id_santri`);
+ALTER TABLE `db_tbl_pelanggaran`
+  ADD CONSTRAINT `db_tbl_pelanggaran_ibfk_1` FOREIGN KEY (`id_santri`) REFERENCES `db_tbl_santri` (`id_santri`);
 
 --
 -- Ketidakleluasaan untuk tabel `db_tbl_softskill`
